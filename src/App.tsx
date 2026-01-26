2144f88ef9fc61ce937789309bb2f36492f0d1c1
Dipanwita Bala <106340756+Dip-Bala@users.noreply.github.com>
2026-01-27 02:24:20 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+function App() {
+
+  return (
+    <div>
+        welsome Welcome to school management dashboard
+        <Button variant="secondary" className="bg-purple-200">Button</Button>
+    </div> 
+  )
+}
+
+export default App
